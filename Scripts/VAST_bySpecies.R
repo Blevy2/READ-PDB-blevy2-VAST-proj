1844e8e9471e9a745f07e16c72e5b7abac312849
Blevy2 <benjamin.levy@noaa.gov>
2023-06-15 15:52:12 -0400

    more generalized script by season, common name, unit etc

--- Scripts/VAST_bySpecies.R
+++ Scripts/VAST_bySpecies.R
@@ -9,13 +9,6 @@ library(dplyr)
 tow_data_wTemp <- readRDS("tow_data_wTemp.RDS")
 
 
-#species names
-S_names <- unique(tow_data_wTemp[c("COMMON_NAME","SPECIES_ITIS","STOCK_ABBREV")])
-
-
-
-
-
 
 #THIS LOOP BREAKS UP THE TOW DATA BY SPECIES
 
@@ -42,15 +35,55 @@ tow_data_species <- readRDS("tow_data_species.RDS")
 
 CN = "COD,ATLANTIC"
 SA = "GBK"
+
+
+
+#species names
+common_names <- unique(tow_data_wTemp[c("COMMON_NAME","SPECIES_ITIS","STOCK_ABBREV")])
+common_names <- c("COD,ATLANTIC")
+
+
+#stock areas 
+stock_areas <- c("GBK")
+
+#VAST obsmodels to use
+obsmodels <- c(7)
+
+#seasons to run
+seasons <- c("FALL")
+
+#use covariates in VAST?
+use_cov <- FALSE
+
+tow_data_season <- list()
+
+for(CN in common_names){
+
+for(SA in stock_areas){
   
 tow_data <- tow_data_species[[CN]][[SA]]
+#use only Temp_Hub values to start
+tow_data = subset(tow_data,!is.na(Temp_Hub))
+
+#use up to 2019 because 2020 missing
+tow_data = subset(tow_data,YEAR<2020)
+
+#replace NA weight values with 0
+tow_data[is.na(tow_data[,"CATCH_WT_CAL"]),"CATCH_WT_CAL"]=0
+range(tow_data$CATCH_WT_CAL)
+
+
+tow_data_season[["SPRING"]] = subset(tow_data,SEASON=="SPRING")
+tow_data_season[["FALL"]] = subset(tow_data,SEASON=="FALL")
 
 
 
 
+for(season in seasons){
 
 
-for(j in c(7)){
+
+for(j in obsmodels){
   
   # OLD ONES FROM CHUCK ADAMS' PAPER WITH CHRIS AND LIZ
   # if(j == 1) {obsmodel <- c(2, 0); run <- 1}
@@ -76,38 +109,27 @@ dir.create(paste(getwd(),"/VAST_runs/",CN,"_",SA,"/obsmodel",j,sep=""))
 setwd((paste(getwd(),"/VAST_runs/",CN,"_",SA,"/obsmodel",j,sep="")))
   
 
-#use only Temp_Hub values to start
-tow_data = subset(tow_data,!is.na(Temp_Hub))
 
-#use up to 2019 because 2020 missing
-tow_data = subset(tow_data,YEAR<2020)
 
-#replace NA weight values with 0
-tow_data[is.na(tow_data[,"CATCH_WT_CAL"]),"CATCH_WT_CAL"]=0
-range(tow_data$CATCH_WT_CAL)
-
-covdata_spring <- tow_data[tow_data$SEASON=="SPRING",c("LATITUDE","LONGITUDE","YEAR","Temp_Hub")]
-colnames(covdata_spring) <- c("Lat","Lon","Year","Temp_Est")
-
-  
 
 # format for use in VAST
-spring <- tow_data %>%
-  filter(SEASON == "SPRING") %>%
+seasonal_tows <- tow_data_season[[season]] %>%
+  #filter(SEASON == as.character(season)) %>%
   # filter(YEAR >= 2009) %>%
   mutate(mycatch = CATCH_WT_CAL) %>%
   select(Year = YEAR,
          Catch_KG = mycatch,
          Lat = LATITUDE,
-         Lon = LONGITUDE) %>%
+         Lon = LONGITUDE,
+         Stratum = STRATUM) %>%
   mutate(Vessel = "missing",
          AreaSwept_km2 =  0.0112*3.4299) #from Chis C- .0112 nm converted to kilometers^2
 
 
-Strata = unique(tow_data$STRATUM)
+Strata = unique(seasonal_tows$Stratum)
 
 
-example <- list(spring)
+example <- list(seasonal_tows)
 example$Region <- "northwest_atlantic"
 example$strata.limits <- data.frame(STRATA = Strata) #THESE ARE COD STRATA
 
@@ -136,39 +158,57 @@ settings <- make_settings(n_x = 500,  #NEED ENOUGH KNOTS OR WILL HAVE ISSUES WIT
 #avoids specific error related to running on server
 #settings$Version <- 'VAST_v12_0_0'
 
+
+
+
+
+ifelse(use_cov==TRUE,
+       
+       #if using covariates
+{covdata <- tow_data_season[[season]][,c("LATITUDE","LONGITUDE","YEAR","Temp_Hub")]
+colnames(covdata) <- c("Lat","Lon","Year","Temp_Est")
 #covariate formula
 X2_formula = ~ poly(Temp_Est, degree=2 ) 
-
-
-
-fit_spring <- fit_model(settings = settings,
-                            "Lat_i"=as.numeric(spring[,'Lat']), 
-                            "Lon_i"=as.numeric(spring[,'Lon']), 
-                            "t_i"=as.numeric(spring[,'Year']), 
-                            "c_iz"=as.numeric(rep(0,nrow(spring))), 
-                            "b_i"=as.numeric(spring[,'Catch_KG']), 
-                            "a_i"=as.numeric(spring[,'AreaSwept_km2']),
+VAST_fit <- fit_model(settings = settings,
+                            "Lat_i"=as.numeric(seasonal_tows[,'Lat']), 
+                            "Lon_i"=as.numeric(seasonal_tows[,'Lon']), 
+                            "t_i"=as.numeric(seasonal_tows[,'Year']), 
+                            "c_iz"=as.numeric(rep(0,nrow(seasonal_tows))), 
+                            "b_i"=as.numeric(seasonal_tows[,'Catch_KG']), 
+                            "a_i"=as.numeric(seasonal_tows[,'AreaSwept_km2']),
                             #   X1_formula = X1_formula,
                             X2_formula = X2_formula,
-                            covariate_data = covdata_spring,
-                            optimize_args=list("lower"=-Inf,"upper"=Inf))
+                            covariate_data = covdata,
+                            optimize_args=list("lower"=-Inf,"upper"=Inf))},
+
+#not using covariates
+{VAST_fit <- fit_model(settings = settings,
+                        "Lat_i"=as.numeric(seasonal_tows[,'Lat']), 
+                        "Lon_i"=as.numeric(seasonal_tows[,'Lon']), 
+                        "t_i"=as.numeric(seasonal_tows[,'Year']), 
+                        "c_iz"=as.numeric(rep(0,nrow(seasonal_tows))), 
+                        "b_i"=as.numeric(seasonal_tows[,'Catch_KG']), 
+                        "a_i"=as.numeric(seasonal_tows[,'AreaSwept_km2']),
+                      
+                        optimize_args=list("lower"=-Inf,"upper"=Inf))}
+)
 
 
 #create directory for season specific output
-dir.create(paste(getwd(),"/spring",sep=""))
-setwd(paste(getwd(),"/spring",sep=""))
+dir.create(paste(getwd(),"/",season,sep=""))
+setwd(paste(getwd(),"/",season,sep=""))
 
 
-saveRDS(fit_spring,file = paste(getwd(),"/fit_spring.RDS",sep=""))
+saveRDS(VAST_fit,file = paste(getwd(),"/VAST_fit_.RDS",season,sep=""))
 
-#  plot_biomass_index(fit_spring)
+#  plot_biomass_index(VAST_fit)
 
-plot(fit_spring)
+plot(VAST_fit)
 
 
 #add years to index csv
 index_csv = read.csv("Index.csv")
-index_csv$Year = as.numeric(fit_spring$year_labels)
+index_csv$Year = as.numeric(VAST_fit$year_labels)
 
 #add season once its in a loop?!?!?!?!
 
@@ -176,4 +216,6 @@ write.csv(index_csv,"Index_wYear.csv")
 
 
 }
-
+}
+}
+}
