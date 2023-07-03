18e37ef795e333f895638ec310929a12c59ecf82
Blevy2 <benjamin.levy@noaa.gov>
2023-07-03 13:59:08 -0400

    updatse to plotting scripts

--- Scripts/Plot_SM_only.R
+++ Scripts/Plot_SM_only.R
@@ -18,9 +18,9 @@ setwd(orig.dir)
 
 
 #all species names
-#tow_data_wTemp <- readRDS("tow_data_wTemp.RDS")
-#common_names <- unique(tow_data_wTemp[c("COMMON_NAME","SPECIES_ITIS","STOCK_ABBREV")])
-#remove(tow_data_wTemp)
+tow_data_wTemp <- readRDS("tow_data_wTemp.RDS")
+common_names <- unique(tow_data_wTemp[c("COMMON_NAME","SPECIES_ITIS","STOCK_ABBREV")])
+remove(tow_data_wTemp)
 
 #specific species names
 #common_names <- c("COD,ATLANTIC")
@@ -40,29 +40,38 @@ common_names =  list.files(paste0(getwd(),"/VAST_runs"))
 #use covariates in VAST?
 #use_cov <- FALSE
 
-pdf(file="Plots_Comparing_Estimates.pdf")
+pdf(file="Plot_Stratified_Mean.pdf")
 
 
-for(CN in common_names){
+for(CN in common_names$COMMON_NAME){
   
- stock_areas =  list.files(paste0(getwd(),"/VAST_runs/",CN))
+ #stock_areas =  common_names$STOCK_ABBREV[common_names$COMMON_NAME==CN]
+ stock_areas = names(strat_mean_species[[CN]])
   
+ #remove unusualy stock areas
+ # if("SFALT" %in% stock_areas){stock_areas=stock_areas[stock_areas!="SFALT"]}
+ # if("GOMWF" %in% stock_areas){stock_areas=stock_areas[stock_areas!="GOMWF"]}
+ # if("GBGOM" %in% stock_areas){stock_areas=stock_areas[stock_areas!="GBGOM"]}
+ 
+ #sometimes unit stocks have other weirder ones
+ if("UNIT" %in% stock_areas){stock_areas=c("UNIT")}
+ 
   for(SA in stock_areas){
 
-    obsmodels =  list.files(paste0(getwd(),"/VAST_runs/",CN,"/",SA))
+    obsmodels =  c(7)
     
 
   for(obsmodel in obsmodels){
     
-      seasons =  list.files(paste0(getwd(),"/VAST_runs/",CN,"/",SA,"/",obsmodel))
+      seasons =  c("FALL","SPRING")
          
-        VAST_fit_unit <- data.frame()
+       
          SM_est_unit <- data.frame()
   
         for(season in seasons){
           
-          covs = list.files(paste0(getwd(),"/VAST_runs/",CN,"/",SA,"/",obsmodel,"/",season))
-          covs = covs[covs %in% c("No_Cov","W_Cov")]
+    #      covs = list.files(paste0(getwd(),"/VAST_runs/",CN,"/",SA,"/",obsmodel,"/",season))
+    #      covs = covs[covs %in% c("No_Cov","W_Cov")]
           
               #pull out stratified mean estimate
           if(season=="SPRING"){SM_est = subset(strat_mean_species[[CN]][[SA]],SEASON=="SPRING")}
@@ -71,27 +80,27 @@ for(CN in common_names){
           #add to overall SM est for unit
           SM_est_unit <- rbind(SM_est_unit,SM_est)
           
-          for(cov in covs){
+         # for(cov in covs){
       
       
     #pull out vast estimate        
-VAST_fit_csv = read.csv(paste0(getwd(),"/VAST_runs/",CN,"/",SA,"/",obsmodel,"/",season,"/",cov,"/Index_wYearSeason.csv"), header=T)
-VAST_fit_csv$SEASON = VAST_fit_csv$Season
+#VAST_fit_csv = read.csv(paste0(getwd(),"/VAST_runs/",CN,"/",SA,"/",obsmodel,"/",season,"/",cov,"/Index_wYearSeason.csv"), header=T)
+#VAST_fit_csv$SEASON = VAST_fit_csv$Season
 
 #add to overall vast dataframe for unit
-VAST_fit_unit <- rbind(VAST_fit_unit,VAST_fit_csv)
+#VAST_fit_unit <- rbind(VAST_fit_unit,VAST_fit_csv)
 
 
-          }
-        }
+         }
+#        }
 
 print(ggplot() +
 
-  #plot VAST estimate with covariates 
-  geom_errorbar(data=VAST_fit_unit,aes(x=Year,y=Estimate,group=SEASON,ymin=Estimate-(1.96*Std..Error.for.Estimate), ymax=Estimate+(1.96*Std..Error.for.Estimate), color = Covariate),width=.3) +
-  geom_point(data=VAST_fit_unit,aes(x=Year,y=Estimate,group=SEASON, color = Covariate),size=2)+
-  geom_line(data=VAST_fit_unit,aes(x=Year,y=Estimate,group=SEASON, color = Covariate))+
-  
+  # #plot VAST estimate with covariates 
+  # geom_errorbar(data=VAST_fit_unit,aes(x=Year,y=Estimate,group=SEASON,ymin=Estimate-(1.96*Std..Error.for.Estimate), ymax=Estimate+(1.96*Std..Error.for.Estimate), color = Covariate),width=.3) +
+  # geom_point(data=VAST_fit_unit,aes(x=Year,y=Estimate,group=SEASON, color = Covariate),size=2)+
+  # geom_line(data=VAST_fit_unit,aes(x=Year,y=Estimate,group=SEASON, color = Covariate))+
+  # 
   #plot stratified calculation data 
   geom_errorbar(data=SM_est_unit,aes(x=YEAR,y=mean.yr.absolute,group=SEASON,ymin=mean.yr.absolute-(1.96*sd.mean.yr.absolute), ymax=mean.yr.absolute+(1.96*sd.mean.yr.absolute), color = "Strat Mean"),width=.3) +
   geom_point(data=SM_est_unit,aes(x=YEAR,y=mean.yr.absolute,group=SEASON, color = "Strat Mean"))+
