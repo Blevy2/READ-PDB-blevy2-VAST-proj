fe465f2ad4505a4018b3329b4db79b14361374ff
Blevy2 <benjamin.levy@noaa.gov>
2023-07-05 10:20:53 -0400

    Chuck's test code

---
+++ run08/run08.R
@@ -0,0 +1,22 @@
+setwd(paste0(getwd(), "/run08"))
+load("svdata.RData")
+
+library(VAST)
+
+settings = make_settings(n_x = 100, Region = "Northwest_Atlantic", purpose = "index2",
+                         strata.limits = list(unique(svdata$stratum)),
+                         n_categories = nlevels(svdata$age))
+ptm <- proc.time()
+fit = fit_model(settings = settings, 
+                Lat_i = svdata$latitude, 
+                Lon_i = svdata$longitude, 
+                t_i = svdata$year, 
+                c_i = as.numeric(svdata$age)-1,
+                b_i = svdata$expcatchwt, 
+                a_i = svdata$AreaSwept_km2)
+proc.time() - ptm
+
+plot(fit)
+
+# Save workspace
+save.image("run08.RData")
